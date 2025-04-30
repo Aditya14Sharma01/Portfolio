3f8495b4e83a04329214d14b48438917fe434453
Aditya Sharma <aditya1401sharma@gmail.com>
2025-04-30 22:37:38 +0530

    update

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function toggleTheme() {
   const currentTheme = document.documentElement.getAttribute('data-theme');
   document.documentElement.setAttribute('data-theme', currentTheme === 'light' ? 'dark' : 'light');
 }
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+const navBackdrop = document.getElementById('navBackdrop');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  navBackdrop.classList.toggle('active');
+});
+
+navBackdrop.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+  navBackdrop.classList.remove('active');
+});
+function toggleMenu() {
+  const links = document.getElementById("navLinks");
+  const backdrop = document.getElementById("navBackdrop");
+  links.classList.toggle("active");
+  backdrop.classList.toggle("active");
+}
